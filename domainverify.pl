afc0617bfdc3d554654e692c46bc3804f1ab66f8
Lars Schotte <gustik@gustik.eu>
2019-10-29 04:03:23 +0100

    looks like newlines are already a part of line strings

--- domainverify.pl
+++ domainverify.pl
@@ -37,6 +37,6 @@ if (not -f $zonefilepath || -z $zonefilepath) {
 
 open(ZONEFILE, "< :encoding(UTF-8)", $zonefilepath);
 while (my $line = <ZONEFILE>) {
-  print $line . "\n";
+  print $line;
 }
 close(ZONEFILE);
